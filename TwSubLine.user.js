835719b4f92610383e3bbd49fb2cf010754a1fda
Personwritep <89448434+personwritep@users.noreply.github.com>
2025-06-22 22:08:25 +0900

    Create TwSubLine.user.js

---
+++ TwSubLine.user.js
@@ -0,0 +1,85 @@
+// ==UserScript==
+// @name        TwSubLine
+// @namespace        http://tampermonkey.net/
+// @version        0.1
+// @description        「X」のタイムライン上のリンクをサブウインドウ表示
+// @author        Everyone
+// @match        https://x.com/*
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=x.com
+// @grant        none
+// @updateURL        https://github.com/personwritep/TwSubLine/raw/main/TwSubLine.user.js
+// @downloadURL        https://github.com/personwritep/TwSubLine/raw/main/TwSubLine.user.js
+// ==/UserScript==
+
+
+
+subline();
+
+
+function subline(){
+    let win_search=location.search;
+    if(win_search.includes('tsl=subline')){
+
+        let style=
+            '<style class="subline">'+
+            'header[role="banner"] { width: 80px; } '+
+            '[data-testid="sidebarColumn"] { display: none; }'+
+            '</style>';
+
+        if(!document.querySelector('.subline')){
+            document.body.insertAdjacentHTML('beforeend', style); }}
+
+} // subline()
+
+
+
+
+document.addEventListener('click', function(event){
+    if(event.shiftKey){
+        event.preventDefault();
+        link_pointer(event); }});
+
+
+setTimeout(()=>{
+    let layers=document.querySelector('#layers');
+    let monitor=new MutationObserver(card_ck);
+    monitor.observe(layers, { childList: true });
+}, 200);
+
+
+function card_ck(){
+    let card=document.querySelector('[data-testid="hoverCardParent"]');
+    if(card){
+        card.addEventListener('click', function(event){
+            if(event.shiftKey){
+                event.preventDefault();
+                link_pointer(event); }});
+    }} // card_ck()
+
+
+
+
+function link_pointer(event){
+    let elem=document.elementFromPoint(event.clientX, event.clientY);
+    if(elem){
+        let link_a=elem.closest('a');
+        if(link_a){
+            event.preventDefault();
+            event.stopImmediatePropagation();
+            let link_url=link_a.getAttribute('href');
+            open_link(link_url);
+        }}}
+
+
+function open_link(link){
+    if(link.startsWith('/')){
+        let url;
+        let link_p=link.split('?');
+        if(link_p.length==1){
+            url='https://x.com'+ link + '?tsl=subline'; }
+        else if(link_p.length==2){
+            url='https://x.com'+ link + '&tsl=subline'; }
+
+        window.open( url, null, 'top=40, left=20, width=720, height=800'); }
+
+} //open_link()
